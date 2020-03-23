c244256e95b42bb405ed00ba6f9af5807d0eba67
mahdiar-021 <62509403+mahdiar-021@users.noreply.github.com>
2020-03-23 19:39:46 +0430

    create passport facility

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Model\Product;
 use Illuminate\Http\Request;
 
+
 class ProductController extends Controller
 {
     /**
@@ -14,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return 'ok';
+        return Product::all();
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('login', 'api\UserController@login');
+Route::post('register', 'api\UserController@register');
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'api\UserController@details');
 });
 
 Route::apiResource('/products','ProductController');
